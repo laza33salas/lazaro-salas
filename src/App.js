71d22794fc2969307f1015255eb0af2e0ef403b1
Lazaro <lazarocode@gmail.com>
2023-01-10 11:41:07 -0300

    install react icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 import Nav from "./components/Nav/Nav.jsx"
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Header />
+      <Main />
     </div>
   );
 }
